ce82123a3caf20f52d52470bc7a5686be990fac2
anthonym01 <samuelmatheson15@gmail.com>
2024-12-31 23:30:52 -0500

    tweak for phones

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 window.addEventListener('load', function () {
-    this.setInterval(function () { createBubble() }, 150);
+    this.setInterval(function () { createBubble() }, 250);
 });
 
 async function createBubble() {
     const bubble = document.createElement('div');
     bubble.classList.add('bubble');
     bubble.style.left = Math.random() * 100 + 'vw';
-    bubble.style.animationDuration = Math.random() * 5 + 8 + 's';
+    bubble.style.animationDuration = Math.random() * 5 + 13 + 's';
     bubble.style.backgroundColor = 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
     document.body.appendChild(bubble);
     bubble.addEventListener('click', () => {
         bubble.style.animationPlayState = 'paused';
-        document.body.removeChild(bubble);
+        bubble.style.backgroundColor = 'hsl(' + Math.random() * 360 + ', 100%,100%)';
+        setTimeout(() => { document.body.removeChild(bubble) }, 500);
     });
     bubble.addEventListener('animationend', () => {
         document.body.removeChild(bubble);
